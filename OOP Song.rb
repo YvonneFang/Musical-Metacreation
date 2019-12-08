154f8f8bdd63440be91928e804e4545a88f47c8a
YvonneFang <45304065+YvonneFang@users.noreply.github.com>
2019-12-08 10:33:50 -0500

    Add files via upload

---
+++ OOP Song.rb
@@ -0,0 +1,41 @@
+# Welcome to Sonic Pi v3.1
+use_bpm = 200
+define :melody do |root, quality|
+  use_synth :blade
+  play_pattern_timed scale(root, quality), 0.5, release: 0.1
+end
+
+define :chords do |root, quality|
+  4.times do
+    play chord(root, quality)
+    sleep 1
+  end
+end
+
+define :bass do |root|
+  2.times do
+    use_synth :tech_saws
+    play root, amp :2
+    sleep 0.5
+  end
+  sleep 3.5
+end
+
+define :beatz do
+  sample :ambi_lunar_land
+end
+
+live_loop :song do
+  root = :C6
+  quality = :minor
+  in_thread do
+    melody(root, quality)
+  end
+  in_thread do
+    bass(root - 24)
+  end
+  in_thread do
+    beatz
+  end
+  chords(root, quality)
+end
---
+++ Recursion Song.rb
@@ -0,0 +1,22 @@
+# Welcome to Sonic Pi v3.1
+define :triangle do |points, depth|
+  counter = 0
+  3.times do
+    use_synth :prophet
+    play points[counter], release: 0.5, amp: 0.5, attack: 2
+    counter += 1
+    sleep 2
+  end
+  if depth > 0
+    depth -= 1
+    triangle [points[0], mid(points[0], points[1]), mid(points[0], points[2])], depth-1
+    triangle [points[1], mid(points[0], points[1]), mid(points[1], points[2])], depth-1
+    triangle [points[2], mid(points[2], points[1]), mid(points[0], points[2])], depth-1
+  end
+end
+
+define :mid do |p1, p2|
+  return (p1[0]+p2[0])/2, (p1[1]+p2[1])/2
+end
+
+triangle([[50, 50], [127, 50], [38.5, 127]], 5)
